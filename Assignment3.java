4c1f36d1fb1646e6114896be312c8028ce2edd39
cyrilbadong34-ui <cyrilbadong34@gmail.com>
2026-09-01 17:51:23 +0800

    Submit Assignment 3

---
+++ Assignment3.java
@@ -0,0 +1,18 @@
+public class Assignment3 { //declares a public class named userInfo
+    public static void main(String[] args) { //the main method is the starting point
+
+        String firstName = "Cyril"; //declares string variable named firstName and stores the first name.
+        String lastName = "Badong"; //declares string variable named lastName and stores the last name.
+        String address = "Sto. Tomas"; //declares string variable named address and stores the address.
+        String mobileNumber = "09617683783"; //declares string variable named mobileNumber and stores the mobile number.
+        int age = 18; //declares int variable named age and stores the first age.
+        double gwa = 93.5; //declares double variable named gwa and stores the GWA with decimal value.
+
+        System.out.println("Firstname: " + firstName); //prints the first name to the console
+        System.out.println("Lastname: " + lastName); //prints the last name to the console
+        System.out.println("Address: " + address); //prints the address to the console
+        System.out.println("Mobile Number: " + mobileNumber); //prints the mobile number to the console
+        System.out.println("Age: " + age); //prints the age to the console
+        System.out.println("GWA: " + gwa); //prints the GWA to the console
+    } //closes the main method
+} //closes the Assignment3 class
